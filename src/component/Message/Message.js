408616ff888e66dde3784a1ed32d930b9dfe51d8
RMKaelBR <116938050+RMKaelBR@users.noreply.github.com>
2023-02-07 21:07:32 +0800

    Message Feature, .json scripts, styling
    Initial code for Message Feature, some scripts from the create-react-app tutorial, and some barebones styling for the messaging feature.

--- src/component/Message/Message.js
+++ src/component/Message/Message.js
@@ -1,8 +1,28 @@
-import React from 'react'
-
 function Message() {
+  let messages1 = []
+  let messages2 = []
   return (
-    <div>Message</div>
+    <div className="messageFeatureDiv">
+      <div className="user1Chat">
+        <input type="text" className="inputChat">
+
+        </input>
+        <button className="sendButton">
+            Send
+        </button>
+      </div>
+      <div className="sharedChatBox">
+        
+      </div>
+      <div className="user2Chat">
+        <input type="text" className="inputChat">
+
+        </input>
+        <button className="sendButton">
+            Send
+        </button>
+      </div>
+    </div>
   )
 }
 
