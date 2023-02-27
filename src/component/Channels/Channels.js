356639760335f5d07de665fbc3724598e3dfe29a
arcferrer5 <arcferrer5@gmail.com>
2023-02-27 17:32:45 +0800

    Minor edits on getChannel func

--- src/component/Channels/Channels.js
+++ src/component/Channels/Channels.js
@@ -39,20 +39,23 @@ const Channels = () => {
       const data = await response.json();
       setChannelArr(data.data);
       console.log(channelArr[0].name);
+      // console.log(data);
     }catch(error){
       console.error(error);
       alert(error.message);
     }
   }
 
+  console.log(channelArr);
+
   return (
     <>
 
     {channelArr.map((channel) => (
-      <div className='channel-main-container' key={channel.id}>
+      <div className='channel-main-container' key={cha.id}>
       <div className='channel-container'>
         <p>{channel.name}</p>
-        <FaTimes className='delete-btn'/>
+        <FaTimes className='delete-btn' onClick={getChannels} />
       </div>
     </div>
     ))}
